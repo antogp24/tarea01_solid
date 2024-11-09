11cc4fe7334df48678cc6e96e6d17d5b8f922dc4
Javier Murillo <jajomuri@espol.edu.ec>
2024-11-08 21:03:47 -0500

    Commit with the aditional benefits

---
+++ Descuento.java
@@ -0,0 +1,10 @@
+
+public class Descuento implements Adicional {
+    // Algún atributo adicional
+    // Algún método específico
+
+    @Override
+    public void confirmar() {
+        // ...
+    }
+}
